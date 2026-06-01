a83323fe87c36312f9c781f340098f6db0649a10
Pradyumna acharya <139623402+2001pradyumnaacharya@users.noreply.github.com>
2026-06-01 11:58:28 +0530

    Add: grumpy-bookstore-owner - submission-11

---
+++ Data Structures & Algorithms/grumpy-bookstore-owner/submission-11.py
@@ -0,0 +1,24 @@
+class Solution:
+    def maxSatisfied(self, customers: List[int], grumpy: List[int], minutes: int) -> int:
+        normal_cust = 0
+        for i in range(len(grumpy)):
+            if grumpy[i] == 0:
+                normal_cust  +=customers[i]
+        i = j = 0 
+        maxi = 0
+        c= 0
+        while j < len(customers):
+            if grumpy[j] == 1:
+                c +=customers[j]
+                # print("the c is", c, "after addding", customers[j])
+
+            if len(customers[i:j +1]) == minutes:
+                # print("length of the len(customers[i:j]) reached ", len(customers[i:j]))
+                maxi = max(maxi, c)
+                # print("Setting max too", maxi)
+                i +=1
+                j = i
+                c =0
+            else:
+                j +=1
+        return normal_cust + maxi
