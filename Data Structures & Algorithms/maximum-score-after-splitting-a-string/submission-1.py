b245fe28610567cc681151a752847f57ea845ce3
Pradyumna acharya <139623402+2001pradyumnaacharya@users.noreply.github.com>
2026-06-03 11:33:19 +0530

    Add: maximum-score-after-splitting-a-string - submission-1

---
+++ Data Structures & Algorithms/maximum-score-after-splitting-a-string/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maxScore(self, s: str) -> int:
+        i , j = 0 , 1
+        ans = []
+        z= 0
+        o = 0 
+
+        while i < len(s) - 1:
+            if s[i] == '0':
+                z +=1
+            while j < len(s):
+                if s[j] == '1':
+                    o +=1
+                j +=1
+            print( "noo is ",z,"and se1 is", o)
+            ans.append(z + o)
+            o = 0
+            i +=1
+            j = i +1
+
+        return max(ans)
+            
