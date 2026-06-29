2dfb75efccfe13f20c3fa90e686106171adc6a45
Pradyumna acharya <139623402+2001pradyumnaacharya@users.noreply.github.com>
2026-06-29 16:17:37 +0530

    Add: average-waiting-time - submission-2

---
+++ Data Structures & Algorithms/average-waiting-time/submission-2.py
@@ -0,0 +1,7 @@
+class Solution:
+    def averageWaitingTime(self, customers: List[List[int]]) -> float:
+        t = total = 0
+        for arrival , order in customers:
+            t = max(t, arrival) + order
+            total += t - arrival
+        return total/ len(customers)
